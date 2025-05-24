b03e7fa7f97dce24b62bf6cfe3b83b406dec50ff
Miguel Veloso Marques Pereira <162155002+MiguelVelosoo@users.noreply.github.com>
2025-05-24 11:38:03 -0300

    mudanças de miguel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { BbSidebarComponent } from './bb-sidebar/bb-sidebar.component';
-import { CardListComponent } from './card-list/card-list.component';
-import { DashboardTopComponent } from './dashboard-top/dashboard-top.component';
-import { DashboardWrapperComponent } from './dashboard-wrapper/dashboard-wrapper.component';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { BbSidebarComponent } from "./bb-sidebar/bb-sidebar.component";
+import { CardListComponent } from "./card-list/card-list.component";
+import { DashboardTopComponent } from "./dashboard-top/dashboard-top.component";
+import { DashboardWrapperComponent } from "./dashboard-wrapper/dashboard-wrapper.component";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   imports: [
     RouterOutlet,
     BbSidebarComponent,
@@ -14,9 +14,9 @@ import { DashboardWrapperComponent } from './dashboard-wrapper/dashboard-wrapper
     DashboardTopComponent,
     DashboardWrapperComponent,
   ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  title = 'senac-riseup-2025';
+  title = "senac-riseup-2025";
 }
