2fd702565443938e41d2261eef3b863d1ef622e4
Kacaii <kacaii.privado@gmail.com>
2025-05-24 07:33:09 -0300

    add top part from dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BbSidebarComponent } from './bb-sidebar/bb-sidebar.component';
 import { CardListComponent } from './card-list/card-list.component';
+import { DashboardTopComponent } from './dashboard-top/dashboard-top.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, BbSidebarComponent, CardListComponent],
+  imports: [
+    RouterOutlet,
+    BbSidebarComponent,
+    CardListComponent,
+    DashboardTopComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/dashboard-top/dashboard-top.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-top',
+  imports: [],
+  templateUrl: './dashboard-top.component.html',
+  styleUrl: './dashboard-top.component.css'
+})
+export class DashboardTopComponent {
+
+}
